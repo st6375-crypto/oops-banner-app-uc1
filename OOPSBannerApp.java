99845e2541f96f597df3d93bfcfaa986d1ef7078
Thanvitha <st6375@srmist.edu.in>
2026-03-02 19:28:17 +0530

    UC1 Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPS Banner App
+ * UC1: Print OOPS to console
+ * @author YourName
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
