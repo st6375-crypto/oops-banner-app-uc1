bb86638d9a2dada3bcba1212a676b0179909bf79
Thanvitha <st6375@srmist.edu.in>
2026-03-02 23:21:22 +0530

    UC2: Render OOPS banner using string concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,17 @@
-/**
- * OOPS Banner App
- * UC1: Print OOPS to console
- * @author YourName
- * @version 1.0
- */
 
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
-}
+        String line1 = "**  **  **  **";
+        String line2 = "*   *  *   *  *   *  *     ";
+        String line3 = "**  **  **  **";
+        String line4 = "*   *  *      *      *   *";
+        String line5 = "**  *      *      **";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}                     
